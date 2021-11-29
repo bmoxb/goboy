1b402fab16af78cb3b8fe1d6f5a24fffeb37d21a
WiredSound <maxoblack@yahoo.com>
2021-11-29 23:09:37 +0000

    define all arithmetic/logic instructions with tests

--- cpu/instruction.go
+++ cpu/instruction.go
@@ -6,47 +6,52 @@ import (
 
 type Operand interface {
 	String() string
+	operand()
 }
 
 type OpReg8 struct{ reg Register8 }
 
 func (o OpReg8) String() string { return o.reg.String() }
+func (o OpReg8) operand()       {}
 
 type OpReg16 struct{ reg Register16 }
 
 func (o OpReg16) String() string { return o.reg.String() }
+func (o OpReg16) operand()       {}
 
 type OpImmediate8 struct{ value uint8 }
 
 func (o OpImmediate8) String() string { return fmt.Sprintf("%d", o.value) }
+func (o OpImmediate8) operand()       {}
 
 type Instruction interface {
 	String() string
 	Cycles() int
 }
 
+//
 // CPU Control
+//
 
 type InstrNop struct{}
 
 func (i InstrNop) String() string { return "nop" }
 func (i InstrNop) Cycles() int    { return 1 }
 
+//
 // Arithmetic/Logic
+//
 
-type InstrAdd struct{ operand1, operand2 Operand }
-
-func (i InstrAdd) String() string { return fmt.Sprintf("add %s, %s", i.operand1, i.operand2) }
-func (i InstrAdd) Cycles() int {
-	switch i.operand1 {
+func binaryArithmeticCycles(operand1, operand2 Operand) int {
+	switch operand1 {
 	case OpReg8{reg: RegA}:
-		switch i.operand2.(type) {
+		switch operand2.(type) {
 		case OpImmediate8:
 			return 2
 		}
 
 		var hl Operand = OpReg16{reg: RegHL{}}
-		if i.operand2 == hl {
+		if operand2 == hl {
 			return 2
 		}
 
@@ -55,28 +60,90 @@ func (i InstrAdd) Cycles() int {
 	case OpReg16{reg: RegHL{}}:
 		return 2
 
-	case RegStackPointer{}:
+	case OpReg16{reg: RegStackPointer{}}:
 		return 4
 	}
 	return 0
 }
 
+type InstrAdd struct{ operand1, operand2 Operand }
+
+func (i InstrAdd) String() string { return fmt.Sprintf("ADD %s, %s", i.operand1, i.operand2) }
+func (i InstrAdd) Cycles() int    { return binaryArithmeticCycles(i.operand1, i.operand2) }
+
 type InstrAdc struct{ operand Operand }
 
-func (i InstrAdc) String() string { return fmt.Sprintf("adc %s", i.operand) }
-func (i InstrAdc) Cycles() int    { return 2 }
+func (i InstrAdc) String() string { return fmt.Sprintf("ADC A, %s", i.operand) }
+func (i InstrAdc) Cycles() int    { return binaryArithmeticCycles(OpReg8{reg: RegA}, i.operand) }
 
-type InstrSub struct{ operand1, operand2 Operand }
+type InstrSub struct{ operand Operand }
 
-func (i InstrSub) String() string { return fmt.Sprintf("sub %s, %s", i.operand1, i.operand2) }
-func (i InstrSub) Cycles() int    { return 2 }
+func (i InstrSub) String() string { return fmt.Sprintf("SUB %s", i.operand) }
+func (i InstrSub) Cycles() int    { return binaryArithmeticCycles(OpReg8{reg: RegA}, i.operand) }
+
+type InstrSbc struct{ operand Operand }
+
+func (i InstrSbc) String() string { return fmt.Sprintf("SBC A, %s", i.operand) }
+func (i InstrSbc) Cycles() int    { return binaryArithmeticCycles(OpReg8{reg: RegA}, i.operand) }
+
+type InstrAnd struct{ operand Operand }
+
+func (i InstrAnd) String() string { return fmt.Sprintf("ADD %s", i.operand) }
+func (i InstrAnd) Cycles() int    { return binaryArithmeticCycles(OpReg8{reg: RegA}, i.operand) }
+
+type InstrOr struct{ operand Operand }
+
+func (i InstrOr) String() string { return fmt.Sprintf("OR %s", i.operand) }
+func (i InstrOr) Cycles() int    { return binaryArithmeticCycles(OpReg8{reg: RegA}, i.operand) }
+
+type InstrXor struct{ operand Operand }
+
+func (i InstrXor) String() string { return fmt.Sprintf("XOR %s", i.operand) }
+func (i InstrXor) Cycles() int    { return binaryArithmeticCycles(OpReg8{reg: RegA}, i.operand) }
+
+type InstrCp struct{ operand Operand }
+
+func (i InstrCp) String() string { return fmt.Sprintf("CP %s", i.operand) }
+func (i InstrCp) Cycles() int    { return binaryArithmeticCycles(OpReg8{reg: RegA}, i.operand) }
+
+func unaryArithmeticCycles(operand Operand) int {
+	switch operand.(type) {
+	case OpReg8:
+		return 1
+	}
+	hl := OpReg16{reg: RegHL{}}
+	if operand == hl {
+		return 3
+	}
+	return 2
+}
 
 type InstrInc struct{ operand Operand }
 
-func (i InstrInc) String() string { return fmt.Sprintf("inc %s", i.operand) }
-func (i InstrInc) Cycles() int    { return 2 }
+func (i InstrInc) String() string { return fmt.Sprintf("INC %s", i.operand) }
+func (i InstrInc) Cycles() int    { return unaryArithmeticCycles(i.operand) }
 
 type InstrDec struct{ operand Operand }
 
-func (i InstrDec) String() string { return fmt.Sprintf("dec %s", i.operand) }
-func (i InstrDec) Cycles() int    { return 2 }
+func (i InstrDec) String() string { return fmt.Sprintf("DEC %s", i.operand) }
+func (i InstrDec) Cycles() int    { return unaryArithmeticCycles(i.operand) }
+
+type InstrDaa struct{}
+
+func (i InstrDaa) String() string { return "DAA" }
+func (i InstrDaa) Cycles() int    { return 1 }
+
+type InstrScf struct{}
+
+func (i InstrScf) String() string { return "SCF" }
+func (i InstrScf) Cycles() int    { return 1 }
+
+type InstrCpl struct{}
+
+func (i InstrCpl) String() string { return "CPL" }
+func (i InstrCpl) Cycles() int    { return 1 }
+
+type InstrCcf struct{}
+
+func (i InstrCcf) String() string { return "CCF" }
+func (i InstrCcf) Cycles() int    { return 1 }
--- cpu/instruction_test.go
+++ cpu/instruction_test.go
@@ -7,7 +7,82 @@ import (
 )
 
 func TestArithmeticLogicInstrCycles(t *testing.T) {
-	assert.Equal(t, InstrAdd{operand1: OpReg8{reg: RegA}, operand2: RegB}.Cycles(), 1)
-	assert.Equal(t, InstrAdd{operand1: OpReg8{reg: RegA}, operand2: OpReg16{reg: RegHL{}}}.Cycles(), 2)
-	assert.Equal(t, InstrAdd{operand1: OpReg16{reg: RegHL{}}, operand2: OpReg16{reg: RegDE{}}}.Cycles(), 2)
+	dataRegA := map[Operand]int{
+		OpReg8{reg: RegA}: 1,
+		OpReg8{reg: RegB}: 1,
+		OpReg8{reg: RegC}: 1,
+		OpReg8{reg: RegD}: 1,
+		OpReg8{reg: RegE}: 1,
+		OpReg8{reg: RegH}: 1,
+		OpReg8{reg: RegL}: 1,
+		// TODO: OpReg16{reg: RegHL{}}:  2,
+		OpImmediate8{value: 0}: 2,
+	}
+
+	dataExtended := map[[2]Operand]int{
+		{OpReg16{reg: RegHL{}}, OpReg16{reg: RegBC{}}}:            2,
+		{OpReg16{reg: RegHL{}}, OpReg16{reg: RegDE{}}}:            2,
+		{OpReg16{reg: RegHL{}}, OpReg16{reg: RegHL{}}}:            2,
+		{OpReg16{reg: RegHL{}}, OpReg16{reg: RegStackPointer{}}}:  2,
+		{OpReg16{reg: RegStackPointer{}}, OpImmediate8{value: 0}}: 4,
+	}
+
+	fullData := map[[2]Operand]int{}
+	for operand, cycles := range dataRegA {
+		fullData[[2]Operand{OpReg8{reg: RegA}, operand}] = cycles
+	}
+	for operands, cycles := range dataExtended {
+		fullData[operands] = cycles
+	}
+
+	// Test ADD instruction (with additional data):
+
+	for operands, cycles := range fullData {
+		instr := InstrAdd{operand1: operands[0], operand2: operands[1]}
+		assert.Equal(t, instr.Cycles(), cycles, "Addition instruction '%s' should take %d cycles", instr, cycles)
+	}
+
+	// Test non-ADD instructions (with just register A data):
+
+	otherBuilders := [](func(Operand) Instruction){
+		func(operand Operand) Instruction { return InstrSub{operand} },
+		func(operand Operand) Instruction { return InstrSbc{operand} },
+		func(operand Operand) Instruction { return InstrAnd{operand} },
+		func(operand Operand) Instruction { return InstrAdc{operand} },
+		func(operand Operand) Instruction { return InstrXor{operand} },
+		func(operand Operand) Instruction { return InstrOr{operand} },
+		func(operand Operand) Instruction { return InstrCp{operand} },
+	}
+	for _, builder := range otherBuilders {
+		for operand, cycles := range dataRegA {
+			instr := builder(operand)
+			assert.Equal(t, instr.Cycles(), cycles, "Arithmetic instruction '%s' should take %d cycles", instr, cycles)
+		}
+	}
+
+	incDecData := map[Operand]int{
+		OpReg8{reg: RegA}: 1,
+		OpReg8{reg: RegB}: 1,
+		OpReg8{reg: RegC}: 1,
+		OpReg8{reg: RegD}: 1,
+		OpReg8{reg: RegE}: 1,
+		OpReg8{reg: RegH}: 1,
+		OpReg8{reg: RegL}: 1,
+		// TODO: OpReg16{reg: RegHL{}}: 3,
+		OpReg16{reg: RegBC{}}: 2,
+		OpReg16{reg: RegDE{}}: 2,
+		// TODO: OpReg16{reg: RegHL{}}: 2,
+		OpReg16{reg: RegStackPointer{}}: 2,
+	}
+
+	incDecBuilder := [](func(Operand) Instruction){
+		func(operand Operand) Instruction { return InstrInc{operand} },
+		func(operand Operand) Instruction { return InstrDec{operand} },
+	}
+	for _, builder := range incDecBuilder {
+		for operand, cycles := range incDecData {
+			instr := builder(operand)
+			assert.Equal(t, instr.Cycles(), cycles, "Increase/decrease instruction '%s' should take %d cycles", instr, cycles)
+		}
+	}
 }
